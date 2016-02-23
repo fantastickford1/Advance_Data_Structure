5c899693c2892869cd0c765f2751b0161632d661
fantastickford1 <carlitos_zenteno@hotmail.com>
2016-02-22 21:45:30 -0600

    Binary Search Tree v.1.5

--- Binary_search_tree/untitled/src/BinarySearchTree.java
+++ Binary_search_tree/untitled/src/BinarySearchTree.java
@@ -1,104 +1,174 @@
 public class BinarySearchTree{
-  Node root;
+    Node root;
+    private int cont = 0;
+    private int nodeHeight;
+    private int height = 0;
 
-  public BinarySearchTree(){
-    root = null;
-  }
-
-  public void insert(int data){
-    Node newNode;
-    newNode = new Node(data);
-    if (root == null) {
-      root = newNode;
-    }else {
-      Node last = null, reco;
-      reco = root;
-      while(reco != null){
-        last = reco;
-        if (data < reco.info) {
-          reco = reco.left;
-        }else{
-          reco = reco.right;
-        }
-      }
-      if (data < last.info) {
-        last.left = newNode;
-      }else {
-        last.right = newNode;
-      }
+    public BinarySearchTree(){
+        root = null;
     }
-  }
-    public boolean delete(int data){
-        if(root == null)
-            return false;
-        else {
-            Node lastNode = null, reco;
+
+    public void insert(int data){
+        Node newNode;
+        newNode = new Node(data);
+        if (root == null) {
+            root = newNode;
+            cont++;
+        }else {
+            Node last = null, reco;
             reco = root;
-            while (reco != null){
-                lastNode = reco;
-                if (data < reco.info) {
+            while(reco != null){
+                last = reco;
+                if (data < reco.info)
                     reco = reco.left;
-                    if (reco.info == data){
-                        if (isLeaf(reco)){
-                            lastNode.left = null;
-                            return true;
-                        }
-                        if (reco.left == null){
-                            lastNode = reco;
-                            reco = reco.right;
-                            lastNode.info = reco.info;
-                            lastNode.right = null;
-                            return true;
-                        }else if (reco.right == null){
-                            lastNode = reco;
-                            reco = reco.left;
-                            lastNode.info = reco.info;
-                            lastNode.left = null;
-                            return true;
-                        }
-                    }
-                }else {
+                else
                     reco = reco.right;
-                    if (reco.info == data){
-                        if (isLeaf(reco)){
-                            lastNode.right = null;
-                            return true;
-                        }
-                        if (reco.left == null){
-                            lastNode = reco;
-                            reco = reco.right;
-                            lastNode.info = reco.info;
-                            lastNode.right = null;
-                            return true;
-                        }else if (reco.right == null){
-                            lastNode = reco;
-                            reco = reco.left;
-                            lastNode.info = reco.info;
-                            lastNode.left = null;
-                            return true;
-                        }
-                    }
-                }
             }
+            if (data < last.info) {
+                cont++;
+                last.left = newNode;
+            }else{
+                cont++;
+                last.right = newNode;
+            }
+        }
+    }
+
+    public boolean delete(int data){
+        if (root != null){
+            Node parent = null;
+            Node current = root;
+            boolean isLeftChild = false;
+            while (current.info != data){
+                parent = current;
+                if (data < current.info) {
+                    isLeftChild = true;
+                    current = current.left;
+                }else {
+                    isLeftChild = false;
+                    current = current.right;
+                }
+                if (current == null)
+                    return false;
+
+            }
+            //The node to delete is a leaf
+            if (isLeaf(current)){
+                if (current == root) {
+                    root = null;
+                    cont = 0;
+                }if (isLeftChild == true)
+                    parent.left = null;
+                else
+                    parent.right = null;
+            }
+            //The node to delete is a branch
+            else if (current.right == null){
+                if (current == root)
+                    root = current.left;
+                else if (isLeftChild)
+                    parent.left = current.left;
+                else
+                    parent.right = current.left;
+            }else if (current.left == null){
+                if (current == root){
+                    root = current.right;
+                }else if (isLeftChild)
+                    parent.left = current.right;
+                else
+                    parent.right = current.right;
+            }else if (!isLeaf(current)){
+                //The branch has two childs
+                Node succssor = getSuccesor(current);
+                if (current == root)
+                    root = succssor;
+                else if (isLeftChild)
+                    parent.left = succssor;
+                else
+                    parent.right = succssor;
+                succssor.left = current.left;
+            }
+            cont--;
+            return true;
         }
         return false;
     }
 
-  public Node search(Node node, int data){
-    if (node == null || node.info == data) {
-      return node;
-    }else if (data < node.info) {
-      return search(node.left, data);
-    }else {
-      return search(node.right, data);
+    private Node getSuccesor(Node deleteNode){
+        Node successor = null;
+        Node successorParent = null;
+        Node current = deleteNode.right;
+        while (current != null){
+            successorParent = successor;
+            successor = current;
+            current = current.left;
+        }
+        if (successor != deleteNode.right){
+            successorParent.left = successor.right;
+            successor.right = deleteNode.right;
+        }
+        return successor;
     }
-  }
 
-  public boolean isLeaf(Node node){
-    return (node.left == null && node.right == null);
-  }
+    public Node search(Node node, int data){
+        if (node == null || node.info == data)
+            return node;
+        else if (data < node.info)
+            return search(node.left, data);
+        else
+            return search(node.right, data);
+    }
 
-  public Node getRoot(){
-    return root;
-  }
+    public boolean isLeaf(Node node){
+        return (node.left == null && node.right == null);
+    }
+
+    public Node getRoot(){
+        return root;
+    }
+
+    public boolean isEmpty(){
+        if (root == null || cont == 0)
+            return true;
+        else return false;
+    }
+
+    public int getNodes(){
+        return cont;
+    }
+
+    public int getNodeHeight(Node node, int id){
+        nodeHeight = 0;
+        return nodeHeight(node,id);
+    }
+
+    private int nodeHeight(Node node, int id){
+        if (node == null || node.info == id)
+            return nodeHeight;
+        else if (id < node.info){
+            nodeHeight++;
+            nodeHeight(node.left, id);
+        }else{
+            nodeHeight++;
+            nodeHeight(node.right, id);
+        }
+        return nodeHeight;
+    }
+
+    public int maxDepth(Node node) {
+        if (node == null) {
+            return 0;
+        } else {
+            /* compute the depth of each subtree */
+            int lDepth = maxDepth(node.left);
+            int rDepth = maxDepth(node.right);
+
+            /* use the larger one */
+            if (lDepth > rDepth) {
+                return (lDepth + 1);
+            } else {
+                return (rDepth + 1);
+            }
+        }
+    }
 }
--- Binary_search_tree/untitled/src/Main.java
+++ Binary_search_tree/untitled/src/Main.java
@@ -4,24 +4,35 @@
 public class Main {
     public static void main(String[] args) {
         BinarySearchTree tree = new BinarySearchTree();
+        System.out.println(tree.isEmpty());
         tree.insert(50);
-        tree.insert(40);
-        tree.insert(60);
-        tree.insert(38);
-        tree.insert(70);
-        tree.insert(47);
-        tree.insert(57);
         tree.insert(48);
-        tree.insert(54);
-        tree.insert(45);
-        tree.insert(59);
+        tree.insert(100);
+        tree.insert(49);
+        tree.insert(32);
+        tree.insert(35);
+        tree.insert(22);
         tree.insert(80);
-        tree.insert(44);
+        tree.insert(102);
+        tree.insert(101);
+        tree.insert(105);
+        tree.insert(83);
+        tree.insert(74);
+        tree.insert(75);
+        tree.insert(72);
+        tree.insert(81);
+        tree.insert(84);
         Node node = tree.search(tree.getRoot(),80);
         if (node == null)
             System.out.println("Cant find the data");
         else
             System.out.println(node.info);
-        tree.delete(45);
+        System.out.println("The node with data 80 is a leaf?: " + tree.isLeaf(node));
+        tree.delete(105);
+        tree.delete(100);
+        System.out.println("The tree is empty? :" + tree.isEmpty());
+        System.out.println("The tree has: " + tree.getNodes() + " Nodes");
+        System.out.println("The node with data 22 is " + tree.getNodeHeight(tree.getRoot(),22) + " height");
+        System.out.print("The tree's height is: " + tree.maxDepth(tree.getRoot()));
     }
 }
